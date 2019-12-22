343478e197077a55127918b537827bda608c2b4a
massvyas <massvyas@gmail.com>
2019-12-21 21:43:52 -0500

    data creation for weekly charts
    Added code to create weekly charts.

---
+++ bokeh_charts_weekly.py
@@ -0,0 +1,235 @@
+# Perform necessary imports
+import sys
+import glob
+import os
+import datetime as dt
+from bokeh.io import curdoc, output_file, show, output_notebook
+from bokeh.plotting import figure
+from bokeh.models import HoverTool, ColumnDataSource, DateRangeSlider, Select
+from bokeh.layouts import widgetbox, row, column
+import pandas as pd
+sys.path.insert(0, 'C:/Users/massv/Dropbox/Trading/Projects/Indicators')
+import symphonie_indicators_v1_0 as sym
+
+csv_files = glob.glob(r'C:\Users\massv\\OneDrive\Documents\Trading\data\*.csv')
+csv_file_names = [os.path.basename(file)[:-4] for file in csv_files]
+
+bars_shown = 200
+
+data = pd.read_csv(r'C:\Users\massv\\OneDrive\Documents\Trading\data\^GSPTSE.csv',parse_dates = True)
+data = data.fillna(method='ffill')
+data['Bar'] = [i for i in range(len(data))]
+data['Colors'] = ['Green' if data.iloc[i].Close > data.iloc[i].Open else 'Red' for i in range(len(data))]
+
+#Fetching symphonie indicator values, removing unwanted columns and renaming indicator columns
+trendline = sym.trendline(data,cciPeriod=63,atrPeriod=18,st=0).drop(['Open','High','Low','Close','trendUp','trendDown','trend'],axis='columns')
+
+emotion = sym.emotion(data,SSP=7,Kmax=50.6).drop(['Open','High','Low','Close'],axis='columns')
+emotion.columns = ['emotionUpInd','emotionDownInd']
+
+extreme = sym.extreme(data, r=12,s=12,u=5).drop(['Open','High','Low','Close','TVI'],axis='columns')
+extreme['extremeUpInd'] = (extreme['TVITrend']==1)*1
+extreme['extremeDownInd'] = (extreme['TVITrend']==-1)*1
+extreme.drop(['TVITrend'],axis='columns', inplace=True)
+
+sentiment = sym.sentiment(data, period=12).drop(['Open','High','Low','Close','g_ibuf_80'],axis='columns')
+sentiment['sentimentUpInd'] = (sentiment['SentimentTrend']==1)*1
+sentiment['sentimentDownInd'] = (sentiment['SentimentTrend']==-1)*1
+sentiment.drop(['SentimentTrend'],axis='columns', inplace=True)
+
+#Merging all symphonie indicators into one df
+symphonie = pd.concat([trendline,emotion,extreme,sentiment],axis=1)
+
+#Assigning colors for up and down indicators
+symphonie['trendUpIndColor'] = ['Blue' if symphonie.iloc[i].trendUpInd == 1 else 'White' for i in range(len(symphonie))]
+symphonie['trendDownIndColor'] = ['Red' if symphonie.iloc[i].trendDownInd == 1 else 'White' for i in range(len(symphonie))]
+symphonie['emotionUpIndColor'] = ['Blue' if symphonie.iloc[i].emotionUpInd == 1 else 'White' for i in range(len(symphonie))]
+symphonie['emotionDownIndColor'] = ['Red' if symphonie.iloc[i].emotionDownInd == 1 else 'White' for i in range(len(symphonie))]
+symphonie['extremeUpIndColor'] = ['Blue' if symphonie.iloc[i].extremeUpInd == 1 else 'White' for i in range(len(symphonie))]
+symphonie['extremeDownIndColor'] = ['Red' if symphonie.iloc[i].extremeDownInd == 1 else 'White' for i in range(len(symphonie))]
+symphonie['sentimentUpIndColor'] = ['Blue' if symphonie.iloc[i].sentimentUpInd == 1 else 'White' for i in range(len(symphonie))]
+symphonie['sentimentDownIndColor'] = ['Red' if symphonie.iloc[i].sentimentDownInd == 1 else 'White' for i in range(len(symphonie))]
+
+#Merging data and symphonie
+data['Date'] = pd.to_datetime(data['Date'])
+data.set_index('Date', inplace=True)
+data = pd.concat([data,symphonie], axis=1)
+
+#Setting dates for default display
+end = max(data.index).date()
+start = end - dt.timedelta(days=bars_shown)
+
+# Make the ColumnDataSource: source
+source = ColumnDataSource(data={
+    'Date'    : data.loc[start:end].index,
+    'Open'    : data.loc[start:end].Open.values,
+    'High'    : data.loc[start:end].High.values,
+    'Low'     : data.loc[start:end].Low.values,
+    'Close'   : data.loc[start:end].Close.values,
+    'Adj Close': data.loc[start:end,['Adj Close']].values,
+    'Bar'     : data.loc[start:end].Bar.values,
+    'Colors'  : data.loc[start:end].Colors,
+    'trendUpIndColor'  : data.loc[start:end].trendUpIndColor,
+    'trendDownIndColor': data.loc[start:end].trendDownIndColor,
+    'emotionUpIndColor'  : data.loc[start:end].emotionUpIndColor,
+    'emotionDownIndColor': data.loc[start:end].emotionDownIndColor,
+    'extremeUpIndColor'  : data.loc[start:end].extremeUpIndColor,
+    'extremeDownIndColor': data.loc[start:end].extremeDownIndColor,
+    'sentimentUpIndColor'  : data.loc[start:end].sentimentUpIndColor,
+    'sentimentDownIndColor': data.loc[start:end].sentimentDownIndColor
+})
+
+
+# Define the callback function: update_plot
+def update_plot(attr, old, new):
+    # Set the yr name to slider.value and new_data to source.data
+    start, end = slider.value
+    start = dt.datetime.fromtimestamp(slider.value[0]/1e3)
+    end = dt.datetime.fromtimestamp(slider.value[1]/1e3)
+    new_data = ColumnDataSource(data={
+        'Date'    : data.loc[start:end].index,
+        'Open'    : data.loc[start:end].Open.values,
+        'High'    : data.loc[start:end].High.values,
+        'Low'     : data.loc[start:end].Low.values,
+        'Close'   : data.loc[start:end].Close.values,
+        'Adj Close': data.loc[start:end,['Adj Close']].values,
+        'Bar'     : data.loc[start:end].Bar.values,
+        'Colors'  : data.loc[start:end].Colors,
+        'trendUpIndColor'  : data.loc[start:end].trendUpIndColor,
+        'trendDownIndColor': data.loc[start:end].trendDownIndColor,
+        'emotionUpIndColor'  : data.loc[start:end].emotionUpIndColor,
+        'emotionDownIndColor': data.loc[start:end].emotionDownIndColor,
+        'extremeUpIndColor'  : data.loc[start:end].extremeUpIndColor,
+        'extremeDownIndColor': data.loc[start:end].extremeDownIndColor,
+        'sentimentUpIndColor'  : data.loc[start:end].sentimentUpIndColor,
+        'sentimentDownIndColor': data.loc[start:end].sentimentDownIndColor
+    }) 
+    source.data.update(new_data.data)
+    
+def update_selector(attr, old, new):
+    #Use global data variable
+    global data
+       
+    # Set the yr name to slider.value and new_data to source.data
+    new_value = select.value
+    idx = csv_file_names.index(new_value)
+    
+    data = pd.read_csv(csv_files[idx],parse_dates = True)
+    data = data.fillna(method='ffill')
+    data['Bar'] = [i for i in range(len(data))]
+    data['Colors'] = ['Green' if data.iloc[i].Close > data.iloc[i].Open else 'Red' for i in range(len(data))]
+
+    #Fetching symphonie indicator values, removing unwanted columns and renaming indicator columns
+    trendline = sym.trendline(data,cciPeriod=63,atrPeriod=18,st=0).drop(['Open','High','Low','Close','trendUp','trendDown','trend'],axis='columns')
+    
+    emotion = sym.emotion(data,SSP=7,Kmax=50.6).drop(['Open','High','Low','Close'],axis='columns')
+    emotion.columns = ['emotionUpInd','emotionDownInd']
+    
+    extreme = sym.extreme(data, r=12,s=12,u=5).drop(['Open','High','Low','Close','TVI'],axis='columns')
+    extreme['extremeUpInd'] = (extreme['TVITrend']==1)*1
+    extreme['extremeDownInd'] = (extreme['TVITrend']==-1)*1
+    extreme.drop(['TVITrend'],axis='columns', inplace=True)
+    
+    sentiment = sym.sentiment(data, period=12).drop(['Open','High','Low','Close','g_ibuf_80'],axis='columns')
+    sentiment['sentimentUpInd'] = (sentiment['SentimentTrend']==1)*1
+    sentiment['sentimentDownInd'] = (sentiment['SentimentTrend']==-1)*1
+    sentiment.drop(['SentimentTrend'],axis='columns', inplace=True)
+    
+    #Merging all symphonie indicators into one df
+    symphonie = pd.concat([trendline,emotion,extreme,sentiment],axis=1)
+    
+    #Assigning colors for up and down indicators
+    symphonie['trendUpIndColor'] = ['Blue' if symphonie.iloc[i].trendUpInd == 1 else 'White' for i in range(len(symphonie))]
+    symphonie['trendDownIndColor'] = ['Red' if symphonie.iloc[i].trendDownInd == 1 else 'White' for i in range(len(symphonie))]
+    symphonie['emotionUpIndColor'] = ['Blue' if symphonie.iloc[i].emotionUpInd == 1 else 'White' for i in range(len(symphonie))]
+    symphonie['emotionDownIndColor'] = ['Red' if symphonie.iloc[i].emotionDownInd == 1 else 'White' for i in range(len(symphonie))]
+    symphonie['extremeUpIndColor'] = ['Blue' if symphonie.iloc[i].extremeUpInd == 1 else 'White' for i in range(len(symphonie))]
+    symphonie['extremeDownIndColor'] = ['Red' if symphonie.iloc[i].extremeDownInd == 1 else 'White' for i in range(len(symphonie))]
+    symphonie['sentimentUpIndColor'] = ['Blue' if symphonie.iloc[i].sentimentUpInd == 1 else 'White' for i in range(len(symphonie))]
+    symphonie['sentimentDownIndColor'] = ['Red' if symphonie.iloc[i].sentimentDownInd == 1 else 'White' for i in range(len(symphonie))]
+    
+    #Merging data and symphonie
+    data['Date'] = pd.to_datetime(data['Date'])
+    data.set_index('Date', inplace=True)
+    data = pd.concat([data,symphonie], axis=1)
+
+    end = max(data.index)
+    start = end - dt.timedelta(days=bars_shown)
+ 
+    # Make the ColumnDataSource: source
+    new_data = ColumnDataSource(data={
+        'Date'    : data.loc[start:end].index,
+        'Open'    : data.loc[start:end].Open.values,
+        'High'    : data.loc[start:end].High.values,
+        'Low'     : data.loc[start:end].Low.values,
+        'Close'   : data.loc[start:end].Close.values,
+        'Adj Close': data.loc[start:end,['Adj Close']].values,
+        'Bar'     : data.loc[start:end].Bar.values,
+        'Colors'  : data.loc[start:end].Colors,
+        'trendUpIndColor'  : data.loc[start:end].trendUpIndColor,
+        'trendDownIndColor': data.loc[start:end].trendDownIndColor,
+        'emotionUpIndColor'  : data.loc[start:end].emotionUpIndColor,
+        'emotionDownIndColor': data.loc[start:end].emotionDownIndColor,
+        'extremeUpIndColor'  : data.loc[start:end].extremeUpIndColor,
+        'extremeDownIndColor': data.loc[start:end].extremeDownIndColor,
+        'sentimentUpIndColor'  : data.loc[start:end].sentimentUpIndColor,
+        'sentimentDownIndColor': data.loc[start:end].sentimentDownIndColor
+    })
+    
+    source.data.update(new_data.data)
+
+# Make a slider object: slider
+slider = DateRangeSlider(title="Date Range: ", start=min(data.index), end=max(data.index), 
+                         value=(start,end), step=1, width=1000)
+
+# Create the figure for the candle stick
+p = figure(title='Line chart', x_axis_label='Dates', y_axis_label='Closing Price', 
+           x_axis_type='datetime', plot_height=400, plot_width=1300)
+p.segment('Bar', 'High', 'Bar', 'Low', source=source, line_width=1, color='black')  # plot the wicks
+p.vbar('Bar', 0.7, 'Close', 'Open', source=source, line_color='black', fill_color = 'Colors')
+
+# Creating the figure for symphonie indicators
+p1 = figure(title='Symphonie indicators', x_axis_type='datetime', plot_height=150, plot_width=1300)
+p1.vbar('Bar', 0.7, 1, 2, source=source, line_color='White', fill_color = 'trendDownIndColor')
+p1.vbar('Bar', 0.7, 2, 3, source=source, line_color='White', fill_color = 'trendUpIndColor')
+
+p1.vbar('Bar', 0.7, 5, 6, source=source, line_color='White', fill_color = 'emotionDownIndColor')
+p1.vbar('Bar', 0.7, 6, 7, source=source, line_color='White', fill_color = 'emotionUpIndColor')
+
+p1.vbar('Bar', 0.7, 9, 10, source=source, line_color='White', fill_color = 'extremeDownIndColor')
+p1.vbar('Bar', 0.7, 10,11, source=source, line_color='White', fill_color = 'extremeUpIndColor')
+
+p1.vbar('Bar', 0.7, 13, 14, source=source, line_color='White', fill_color = 'sentimentDownIndColor')
+p1.vbar('Bar', 0.7, 14, 15, source=source, line_color='White', fill_color = 'sentimentUpIndColor')
+
+hover = HoverTool(tooltips=[
+        ('bar', '@Bar'),
+        ('date', '@Date{%F %T}'),
+        ('open', '@Open{0.0000f}'),
+        ('high', '@High{0.0000f}'),
+        ('low', '@Low{0.0000f}'),
+        ('close', '@Close{0.0000f}')
+    ],
+        formatters={'Date': 'datetime'})
+p.add_tools(hover)
+
+# Attach the callback to the 'value' property of slider
+slider.on_change('value',update_plot)
+
+# Create a dropdown Select widget for the x data: x_select
+select = Select(
+    options=csv_file_names,
+    value='^GSPTSE',
+    title='Stock Selector'
+)
+
+select.on_change('value', update_selector)
+
+# Add the plot to the current document and add a title
+layout = column(row(select, widgetbox(slider)), p, p1)
+curdoc().add_root(layout)
+curdoc().title = 'Stock Chart'
+
+# Output the file and show the figure
+#output_file('chart.html')
+#show(layout)
--- update_stock_prices.py
+++ update_stock_prices.py
@@ -5,10 +5,18 @@ import datetime
 stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\GitHub\bokeh_equity_charts\bokeh_equity_charts\stock_list.csv')
 
 for i in range(len(stock_list)):
-    df = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\\' + stock_list.loc[i]['Stock_Name'] + '.csv', parse_dates=True, index_col='Date')
+    df = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\Daily\\' + stock_list.loc[i]['Stock_Name'] + '.csv', parse_dates=True, index_col='Date')
     start = max(df.index) + datetime.timedelta(days=1)
     end = datetime.date.today()
+    df_weekly = pd.DataFrame(columns=['Open','High','Low','Close','Adj Close','Volume'])
     if end >= start.date():
         new = web.DataReader(stock_list.loc[i]['Stock_Name'], 'yahoo', start, end)
         df = pd.concat([df,new], axis=0)
-        df.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\\' + stock_list.loc[i]['Stock_Name'] + '.csv')
+        df_weekly['Open'] = df.resample('W')['Open'].first()
+        df_weekly['High'] = df.resample('W')['High'].max()
+        df_weekly['Low'] = df.resample('W')['Low'].min()
+        df_weekly['Close'] = df.resample('W')['Close'].last()
+        df_weekly['Adj Close'] = df.resample('W')['Adj Close'].last()
+        df_weekly['Volume'] = df.resample('W')['Volume'].sum()
+        df.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\Daily\\' + stock_list.loc[i]['Stock_Name'] + '.csv')
+        df_weekly.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\Weekly\\' + stock_list.loc[i]['Stock_Name'] + '.csv')
