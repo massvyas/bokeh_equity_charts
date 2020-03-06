951f15b931164ef6b068c0c2d433eb796898f81e
massvyas <massvyas@gmail.com>
2020-03-05 22:37:16 -0500

    Updates to data download
    Updated data download code

--- download_stock_prices.py
+++ download_stock_prices.py
@@ -4,14 +4,14 @@ import datetime
 
 stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\GitHub\bokeh_equity_charts\bokeh_equity_charts\stock_list.csv')
 
-start = datetime.datetime(1990, 1, 1)
-end = datetime.datetime(2019,12, 18)
-#end = datetime.date.today()
+start = datetime.datetime(2019, 2,27)
+#end = datetime.datetime(2019,12, 18)
+end = datetime.date.today()
 
 for i in range(len(stock_list)):
     df = web.DataReader(stock_list.loc[i]['Stock_Name'], 'yahoo', start, end)
     df.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\\' + stock_list.loc[i]['Stock_Name'] + '.csv')
     
 #Download data for individual stocks
-df = web.DataReader('^GSPTSE', 'yahoo', start, end)
-df.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\^GSPTSE.csv')
+df = web.DataReader('ASHOKLEY.NS', 'yahoo', start, end)
+df.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\ASHOLKEY_2019.csv')
