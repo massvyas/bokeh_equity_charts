f6c9d3b1fab17efd104768acb321470ae04cef85
massvyas <massvyas@gmail.com>
2020-06-02 22:47:18 -0400

    updated equity lists
    Removed equity list files and updated code to download NSE data

--- download_stock_prices.py
+++ download_stock_prices.py
@@ -2,15 +2,15 @@ import pandas_datareader.data as web
 import pandas as pd
 import datetime
 
-stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\GitHub\bokeh_equity_charts\bokeh_equity_charts\stock_list.csv')
+stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\nse_stock_list.csv')
 
-start = datetime.datetime(2019, 2,27)
+start = datetime.datetime(1995, 1,1)
 #end = datetime.datetime(2019,12, 18)
 end = datetime.date.today()
 
 for i in range(len(stock_list)):
     df = web.DataReader(stock_list.loc[i]['Stock_Name'], 'yahoo', start, end)
-    df.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\\' + stock_list.loc[i]['Stock_Name'] + '.csv')
+    df.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\Daily\\' + stock_list.loc[i]['Stock_Name'] + '.csv')
     
 #Download data for individual stocks
 df = web.DataReader('ASHOKLEY.NS', 'yahoo', start, end)
--- update_stock_prices.py
+++ update_stock_prices.py
@@ -2,7 +2,7 @@ import pandas_datareader.data as web
 import pandas as pd
 import datetime
 
-stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\GitHub\bokeh_equity_charts\stock_list.csv')
+stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\GitHub\bokeh_equity_charts\tse_stock_list.csv')
 
 for i in range(len(stock_list)):
     df = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\Daily\\' + stock_list.loc[i]['Stock_Name'] + '.csv', parse_dates=True, index_col='Date')
@@ -23,7 +23,7 @@ for i in range(len(stock_list)):
         df_weekly.to_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\Weekly\\' + stock_list.loc[i]['Stock_Name'] + '.csv')
         
 #Remove duplicate entries or entries with duplicate row index
-stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\GitHub\bokeh_equity_charts\stock_list.csv')
+stock_list = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\GitHub\bokeh_equity_charts\tse_stock_list.csv')
 
 for i in range(len(stock_list)):
     df = pd.read_csv(r'C:\Users\massv\OneDrive\Documents\Trading\data\Daily\\' + stock_list.loc[i]['Stock_Name'] + '.csv', parse_dates=True, index_col='Date')
